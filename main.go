5c1eb132f76d4fc5519c1a7595725130376d3c73
Ivelin Dzhangarov <djangarov.i@gmail.com>
2022-12-19 18:00:10 +0200

    + Show uploaded images

--- main.go
+++ main.go
@@ -24,12 +24,21 @@ func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/fileupload", fileuploadHandler)
 	http.HandleFunc("/favicon.ico", faviconHandler)
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 func indexHandler(w http.ResponseWriter, req *http.Request) {
 	sessionCookie := getSessionCookie(w, req)
-	tpl.ExecuteTemplate(w, "index.gohtml", sessionCookie)
+	imgs := getImages(w, req)
+	tplValues := struct {
+		SessionValue string
+		Images       []string
+	}{
+		SessionValue: sessionCookie.Value,
+		Images:       imgs,
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", tplValues)
 }
 
 func fileuploadHandler(w http.ResponseWriter, req *http.Request) {
@@ -43,9 +52,11 @@ func fileuploadHandler(w http.ResponseWriter, req *http.Request) {
 		fname := generateFileSHAname(mf, fh)
 
 		saveFile(mf, fname)
+
+		saveFileCookie(w, req, fname)
 	}
 
-	http.Redirect(w, req, req.Header.Get("Referer"), 302)
+	http.Redirect(w, req, req.Header.Get("Referer"), http.StatusFound)
 }
 
 func faviconHandler(w http.ResponseWriter, req *http.Request) {
@@ -67,8 +78,19 @@ func createSessionCookie(w http.ResponseWriter, req *http.Request) *http.Cookie 
 		Name:  "session",
 		Value: sId.String(),
 	}
+	http.SetCookie(w, c)
 
 	return c
+}
+
+func getImages(w http.ResponseWriter, req *http.Request) []string {
+	c := getFileCookie(w, req)
+	var imgs []string
+	if len(c.Value) > 0 {
+		imgs = strings.Split(c.Value, "|")
+	}
+
+	return imgs
 }
 
 func generateFileSHAname(mf multipart.File, fh *multipart.FileHeader) string {
@@ -94,4 +116,43 @@ func saveFile(mf multipart.File, fileName string) {
 
 	mf.Seek(0, 0)
 	io.Copy(nf, mf)
+}
+
+func saveFileCookie(w http.ResponseWriter, req *http.Request, fileName string) *http.Cookie {
+	c := getFileCookie(w, req)
+
+	appendCookieValue(w, c, fileName)
+
+	return c
+}
+
+func getFileCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("files")
+	if err != nil {
+		c = createFileCookie(w, req)
+	}
+
+	return c
+}
+
+func createFileCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c := &http.Cookie{
+		Name: "files",
+	}
+
+	return c
+}
+
+func appendCookieValue(w http.ResponseWriter, c *http.Cookie, fileName string) *http.Cookie {
+	s := c.Value
+	if !strings.Contains(s, fileName) {
+		if len(s) != 0 {
+			s += "|"
+		}
+		s += fileName
+	}
+
+	c.Value = s
+	http.SetCookie(w, c)
+	return c
 }
